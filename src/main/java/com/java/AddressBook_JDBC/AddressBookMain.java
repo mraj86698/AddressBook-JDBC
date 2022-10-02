1c975c716996d131971e82d7535440cbdc016481
MohanRaj <mraj86698@gmail.com>
2022-10-02 19:51:13 +0530

    Added to Welcome Message

---
+++ src/main/java/com/java/AddressBook_JDBC/AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.java.AddressBook_JDBC;
+
+/**
+ * Hello world!
+ *
+ */
+public class AddressBookMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Address Book Using JDBC!" );
+    }
+}
---
+++ src/test/java/com/java/AddressBook_JDBC/AddressBookMainTest.java
@@ -0,0 +1,15 @@
+package com.java.AddressBook_JDBC;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AddressBookMainTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
